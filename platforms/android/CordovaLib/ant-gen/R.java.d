cc1da1e59916f7b32d1e8447671d4fbbe3ef0ef1
Bart Akeley <bart.akeley@gmail.com>
2014-11-17 22:54:54 -0600

    remove platforms from git

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /home/bartronx7/gits/workoutbook/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ platforms/android/ant-gen/org/workoutbook/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.workoutbook;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
